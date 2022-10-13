f7ca5a11903accb8044f6a007cf945798943875f
Jade <jadeline.lu.08@gmail.com>
2022-10-13 19:04:53 +0200

    added images to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Hero from "./Components/Hero";
 import RegisterForm from "./Components/RegisterForm";
 
 function App() {
---
+++ src/Components/HeroRight.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function HeroRight() {
+  return <Container>Här ska videon vara!</Container>;
+}
+
+export default HeroRight;
+const Container = styled.div`
+  display: grid;
+  margin-top: 15%;
+  margin-left: 15%;
+`;
