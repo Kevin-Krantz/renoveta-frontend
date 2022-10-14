11750476dfbc7c8d282fb14767cdcae08e6c7c23
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-10-14 13:17:20 +0200

    Imported logo for the footer, also built the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import styled from "styled-components";
 import Footer from "./components/Footer";
 
 function App(): JSX.Element {
-  return <Footer />;
+  return (
+    <>
+      <Wrapper>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+        <h1>TESTING</h1>
+      </Wrapper>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  min-height: 45vmax;
+`;
