555b78e532a63881dd0cf33bfd33c53e7f621016
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-10-16 19:52:52 +0200

    Made Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App(): JSX.Element {
   return (
     <>
+      <Navbar />
       <Wrapper>
         <h1>TESTING</h1>
         <h1>TESTING</h1>
