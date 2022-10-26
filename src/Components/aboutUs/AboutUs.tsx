fed9e86a31016bbe0517b0b6968718f0974d5dd3
Jade <jadeline.lu.08@gmail.com>
2022-10-26 12:37:55 +0200

    added reset.css

--- src/Components/aboutUs/AboutUs.tsx
+++ src/Components/aboutUs/AboutUs.tsx
@@ -7,7 +7,7 @@ import Sofia from "./Sofia";
 function AboutUs() {
   return (
     <Wrapper>
-      <h1>
+      <p>
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -15,20 +15,23 @@ function AboutUs() {
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
         mollit anim id est laborum."
-      </h1>
+      </p>
       <Linus />
       <Elin />
       <Maximillian />
       <Sofia />
-      <h1>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
-      </h1>
+      <End>
+        <img src="images\pink-rectangle-right.png" />
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </End>
     </Wrapper>
   );
 }
@@ -37,9 +40,31 @@ export default AboutUs;
 
 const Wrapper = styled.div`
   display: grid;
-  height: 500vh;
+  height: 600vh;
   width: 100%;
-  h1 {
+  color: var(--text-secondary);
+  position: relative;
+  p {
     text-align: center;
+    margin: 0 !important;
+    display: inline-block;
+  }
+`;
+
+const End = styled.div`
+  position: relative;
+  text-align: left;
+
+  img {
+    position: absolute;
+    bottom: 0;
+    width: 70%;
+    height: 100%;
+  }
+
+  p {
+    position: absolute;
+    top: 100px;
+    left: 8px;
   }
 `;
