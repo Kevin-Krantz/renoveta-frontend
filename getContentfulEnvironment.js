e9fa75acae8ae4fedd160cff157a485dfb97c801
Jade <jadeline.lu.08@gmail.com>
2022-11-07 10:26:10 +0100

    added dotenv file to access key to contenfuk

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -1,15 +1,21 @@
-import { createClient } from "contentful";
+//skapat client till att hämta content från contenful
 import contentfulManagement from "contentful-typescript-codegen";
 
-export default function () {
-  //  createClient({
-  //     space: "dd2qfg19yu3v",
-  //     accessToken: "LHKZjOWJdq1rRmZEdTIRsJG-vfukKGVV2sUfWjYt9zo",
-  //   });
-
-  const client = contentfulManagement.createClient({
-    accessToken: "LHKZjOWJdq1rRmZEdTIRsJG-vfukKGVV2sUfWjYt9zo",
+module.exports = function () {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
   });
 
-  return client.getSpace("dd2qfg19yu3v");
-}
+  return contentfulClient
+    .getSpace(process.env.CONTENTFUL_SPACE_ID)
+    .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT));
+};
+
+// console.log("process", process.env);
+// export default function () {
+//   const client = contentfulManagement.createClient({
+//     accessToken: "LHKZjOWJdq1rRmZEdTIRsJG-vfukKGVV2sUfWjYt9zo",
+//   });
+
+//   return client.getSpace("dd2qfg19yu3v");
+// }
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -3,15 +3,14 @@ import { useState, useEffect } from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
-  space: "dd2qfg19yu3v",
-  accessToken: "LHKZjOWJdq1rRmZEdTIRsJG-vfukKGVV2sUfWjYt9zo",
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || "",
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_KEY || "",
 });
 
 function FAQ() {
-  const [faqs, setFaqs] = useState<any>([]);
+  const [faqs, setFaqs] = useState<any[]>([]);
 
   useEffect(() => {
-    // setFaqs();
     async function loadEntries() {
       const { items } = await client.getEntries({ content_type: "faq" });
       setFaqs(items as any);
@@ -22,8 +21,7 @@ function FAQ() {
 
   return (
     <div>
-      {/* @ts-disable */}
-      {faqs.map((faq: any) => (
+      {faqs.map((faq) => (
         <div key={faq.sys.id}>
           <h3>{faq.fields.title}</h3>
           <h3>{documentToReactComponents(faq.fields.text)}</h3>
