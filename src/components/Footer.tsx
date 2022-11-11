dcc67d03e87976ba3cb27f89bde5725967097e9c
Jade <jadeline.lu.08@gmail.com>
2022-11-11 11:02:44 +0100

    Footer fixed media queries and rename the links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,61 +1,69 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Footer(): JSX.Element {
   return (
-    <Box>
-      <Container>
-        <img alt="" src="/images/Renovetalogo.png" />
-        <Row>
-          <Heading>
-            <h1>Ditt digitala verktyg för att planera din renovering</h1>
-          </Heading>
-          <VerticalLine></VerticalLine>
-          <Column>
-            <FooterLink href="#">
-              <h2>Om Renoveta</h2>
-            </FooterLink>
-            <FooterLink href="#">
-              <h2>Vad är Renoveta?</h2>
-            </FooterLink>
-            <FooterLink href="#">
-              <h2>Hur fungerar det?</h2>
-            </FooterLink>
-            <FooterLink href="#">
-              <h2>Kontakta oss</h2>
-            </FooterLink>
-            <br />
-            <br />
-            <FooterLink className="nounderline" href="#">
-              <h2>Följ oss på sociala medier</h2>
-            </FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </Box>
+    <Container>
+      <img alt="" src="/images/Renovetalogo.png" />
+      <Row>
+        <Heading>
+          <h1>Ditt digitala verktyg för att planera din renovering</h1>
+        </Heading>
+        <VerticalLine></VerticalLine>
+        <Column>
+          <FooterLink>
+            <h2>
+              <Link className="Link" to="">
+                Varför Renoveta?
+              </Link>
+            </h2>
+          </FooterLink>
+          <FooterLink>
+            <h2>
+              <Link className="Link" to="">
+                Tjänsten
+              </Link>
+            </h2>
+          </FooterLink>
+          <FooterLink>
+            <h2>
+              <Link className="Link" to="/aboutus">
+                Om oss
+              </Link>
+            </h2>
+          </FooterLink>
+          <FooterLink>
+            <h2>
+              <Link className="Link" to="/faq">
+                Q&A
+              </Link>
+            </h2>
+          </FooterLink>
+          <br />
+          <br />
+          <FooterLink className="nounderline">
+            <h2>Följ oss på sociala medier</h2>
+          </FooterLink>
+        </Column>
+      </Row>
+    </Container>
   );
 }
 
 export default Footer;
 
-const Box = styled.div`
+const Container = styled.div`
+  height: 550px;
   background: #c8e5d9;
   padding-bottom: 40px;
   padding-top: 40px;
   position: relative;
-`;
+  color: var(--text-secondary) !important;
 
-const Container = styled.div`
-  @media all and (max-width: 27in) {
+  @media screen and (max-width: 900px) {
     img {
-      height: 120px;
-      margin-left: 455px;
-    }
-  }
-
-  @media all and (max-width: 25in) {
-    img {
-      height: 120px;
-      margin-left: 240px;
+      height: 100px;
+      width: 300px;
     }
   }
 `;
@@ -71,19 +79,18 @@ const Row = styled.div`
   justify-content: space-evenly;
   place-items: flex-start;
 
-  @media (max-width: 1000px) {
+  @media screen and (max-width: 900px) {
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   }
 `;
 
 const FooterLink = styled.a`
   font-size: 18px;
-  color: #1a5a4b;
   text-underline-offset: 5px;
   line-height: 2.5em;
 
-  &.nounderline {
-    text-decoration: none;
+  .Link {
+    color: var(--text-secondary);
   }
 
   h2 {
@@ -92,24 +99,39 @@ const FooterLink = styled.a`
   }
 
   &:hover {
-    color: #ffffff;
     transition: 50ms ease-in;
+  }
+
+  @media screen and (max-width: 900px) {
+    font-size: 14px;
+    h2 {
+      margin-right: 25px;
+    }
   }
 `;
 
 const Heading = styled.div`
   font-size: 25px;
-  color: #1a5a4b;
 
   h1 {
     font-weight: 700;
     line-height: 1.2em;
     width: 552px;
   }
+
+  @media screen and (max-width: 900px) {
+    font-size: 18px;
+
+    h1 {
+      width: 350px;
+      margin-left: 10px;
+      margin-right: 25px;
+    }
+  }
 `;
 
 const VerticalLine = styled.div`
-  border-left: 1px solid #1a5a4b;
+  border-left: 1px solid var(--text-secondary);
   height: 350px;
   position: absolute;
   top: 70px;
