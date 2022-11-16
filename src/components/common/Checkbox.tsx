960f5716ae40184d06445ef0414f5dda5124757e
Jade <jadeline.lu.08@gmail.com>
2022-11-16 18:13:56 +0100

    small fixes

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 
 interface CheckboxProps {
-id?: number
-handleCheck?(event:React.ChangeEvent<HTMLInputElement>): void
-isChecked?: boolean
-value?:string
-className?: string
-name: string;
+  id?: number;
+  handleCheck?(event: React.ChangeEvent<HTMLInputElement>): void;
+  isChecked?: boolean;
+  value?: string;
+  className?: string;
+  name: string;
 }
 function Checkbox(props: CheckboxProps) {
-    return (
-        <li>
-            <input
-            key={props.id}
-            onChange ={props.handleCheck}
-            type="checkbox"
-            checked={props.isChecked}
-            value={props.value}
-            className={props.className}
-            name={props.name}
-            />
-            {props.name}
-        </li>
-            
-        
-    );
+  return (
+    <li>
+      <input
+        key={props.id}
+        onChange={props.handleCheck}
+        type="checkbox"
+        checked={props.isChecked}
+        value={props.value}
+        className={props.className}
+        name={props.name}
+      />
+      {props.name}
+    </li>
+  );
 }
 
 export default Checkbox;
