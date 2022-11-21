47a528c1482c7299b711f2d587499d61adc6ec5e
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-11-21 04:50:20 +0100

    corrected import

--- src/MyPages/MyPage.tsx
+++ src/MyPages/MyPage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { IUser } from "../types/User";
-import Form from "./FormsTable";
+import FormsTable from "./FormsTable";
 
 interface Props {
   user?: IUser;
@@ -11,7 +11,7 @@ function MyPage({ user }: Props) {
       <h1>{"Hej! " + user?.name}</h1>
       {user?.isAdmin && <Title>Ärenden</Title>}
       {!user?.isAdmin && <Title>Dina pågående ärenden</Title>}
-      <Form user={user} />
+      <FormsTable user={user} />
     </Container>
   );
 }
