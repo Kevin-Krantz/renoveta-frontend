1fe9296edbc50b0df3aa23a68b5fa02c0c616ddf
Jade <jadeline.lu.08@gmail.com>
2022-11-28 11:21:08 +0100

    fixed in userservice to redirect after register

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,13 +1,10 @@
 import http from "./httpService";
-import auth from "./authService";
 import config from "../config.json";
 
 const userApi = config.apiEndpoint + "/users";
 
-async function register(user: any) {
-  const { data, headers } = await http.post(userApi, user);
-  auth.loginWithJwt(headers["x-auth-token"]);
-  return data;
+function register(user: any) {
+  return http.post(userApi, user);
 }
 
 export default {
